5568c047f6272a44aa23e964b493ea2f4d659f51
Kuanysh <tlenshikuanysh@gmail.com>
2024-01-30 09:24:32 +0600

    add lab-2

---
+++ lab-2/LIsts.py
@@ -0,0 +1,31 @@
+#EXERCISE1
+fruits = ["apple", "banana", "cherry"]
+print(fruits[1])
+
+#EXERCISE2
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+
+#EXERCISE3
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+#EXERCISE4
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1,"lemon")
+
+#EXERCISE5
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+
+#EXERCISE6
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+#EXERCISE7
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+#EXERCISE8
+
+#EXERCISE9
